0545f2b175eb9d5e8739c8a0006e9c39d2ee5e45
OdysseyScorpio <martyn.robert.green@gmail.com>
2017-02-21 11:40:41 +0000

    me

--- src/test/java/Datalayer/Booking/BooksSteats/TestData.java
+++ src/test/java/Datalayer/Booking/BooksSteats/TestData.java
@@ -3,8 +3,15 @@ package test.java.Datalayer.Booking.BooksSteats;
 import main.java.Datalayer.Booking.BooksSteats.Screen;
 
 public class TestData {
-	private Screen testScreen = new Screen();
+ 
+	
+	private void testMethod() {
+		
+		Screen testScreen = new Screen(1,2,"1");
 
-
+		int x = testScreen.getCapacity();
+		System.out.println(x);
+		
+	}
 
 }
