0744c99f4134c2e2332bbd95ee5b8b54e9bb9e02
Alex <dusk3d@protonmail.ch>
2017-02-21 15:32:58 +0000

    classification

--- src/main/java/BS/QACinema/dataLayer/film/Film.java
+++ src/main/java/BS/QACinema/dataLayer/film/Film.java
@@ -2,9 +2,9 @@ package BS.QACinema.dataLayer.film;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.*;
 
@@ -34,9 +34,9 @@ public class Film {
 	private int date;
 	
 	@NotNull
-	@ManyToOne
-	@JoinColumn(name = "ClassificationID", nullable = false)
-	private String classification;
+	@Column(name = "Classification", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Classification classification;
 
 	public Film(String title, int lengthMins, String description, int date) {
 		super();
@@ -78,8 +78,12 @@ public class Film {
 		this.date = date;
 	}
 	
-	public String getClassification(){
+	public Classification getClassification(){
 		return classification;
+	}
+	
+	public void setClassification(Classification classi){
+		this.classification = classi;
 	}
 
 }
