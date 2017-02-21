2ffa206acc3a8d0e0e916fa7e2be2d3b303e3c44
jetbooster <jarvisam@gmail.com>
2017-02-21 15:16:15 +0000

    added Classification, Genre, Media

--- src/main/java/BS/QACinema/dataLayer/film/Media.java
+++ src/main/java/BS/QACinema/dataLayer/film/Media.java
@@ -1,11 +1,34 @@
 package BS.QACinema.dataLayer.film;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity @Table
 public class Media {
 	
+	@NotNull
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(nullable = false, unique = true)
 	private int mediaID;
+	
+	@NotNull
+	@Column (nullable = false)
 	private String url;
+	
 	public enum MediaType{IMAGE,VIDEO,SOUNDCLIP};
+	
+	@NotNull
+	@Column (nullable = false)
 	private MediaType mediaType;
+	
+	@NotNull
+	@Column (nullable = false)
 	private int filmID;
 	
 	
