8e2381cba7f61ce9b0158084a8d16a60d3c06eb8
JamesLam1990 <james.lamkin@hotmail.co.uk>
2017-02-22 11:24:15 +0000

    Showing Entitys

--- src/main/java/qa_cinema/manager/ShowingManager.java
+++ src/main/java/qa_cinema/manager/ShowingManager.java
@@ -6,11 +6,14 @@ import qa_cinema.data.film.Film;
 
 public interface ShowingManager {
 	
+	public List<Showing> allShowings();
 	public List<Showing> findByHour(String hour);
 	public List<Showing> findByDay(String date);
 	public List<Showing> findByFilm(Film film);
 	public List<Showing> findByfilm(String film);
-	public List<Showing> allShowings();
+	public List<Showing> byExperience(String experience);
+	public List<Showing> byAccessabillity(String accessability);
+	
 	
 }
 
