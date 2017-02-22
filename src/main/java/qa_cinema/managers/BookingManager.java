d2415e83b5687144a66738e09f48a146d4e48afd
dskhd <dusk3d@protonmail.ch>
2017-02-22 10:48:01 +0000

    bookingmanager list tickets

--- src/main/java/qa_cinema/managers/BookingManager.java
+++ src/main/java/qa_cinema/managers/BookingManager.java
@@ -4,13 +4,14 @@ package qa_cinema.managers;
 import java.util.List;
 
 import qa_cinema.data.booking.Booking;
+import qa_cinema.data.booking.tickets.Ticket;
 
 public interface BookingManager {
 
 	public Booking persistBooking(Booking booking);
 	public Booking findByBookingId(int bookingid);
 	public List<Booking> findByEmail(String email);
-	public List<Booking> findAllBookings();
+	public List<Ticket> findAllTickets(Booking booking);
 	public List<Booking> findByPaymentID(int paymentid);
 	public void updateBooking(Booking booking);
 	public void deleteBooking(Booking booking);
