56565ceb1087c10c3fac63a1e4d86ed103a6b623
Mark-R-Lester <mrlester@live.co.uk>
2017-02-23 12:04:15 +0000

    all

--- src/main/java/qacinema/data/users/User.java
+++ src/main/java/qacinema/data/users/User.java
@@ -56,7 +56,7 @@ public class User {
 	      name="user_has_addresses",
 	      joinColumns = @JoinColumn(name="users_email", referencedColumnName="ID"),
 	      inverseJoinColumns = @JoinColumn(name="address_addressId", referencedColumnName="ID"))
-	private List<Address> addressess;
+	private List<Address> addresses;
 	
 	
 	
