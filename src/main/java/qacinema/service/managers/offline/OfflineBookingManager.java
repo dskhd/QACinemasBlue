ce16dcfe2b972fc37ff2f844b2fd9df9e1ab18ff
dskhd <dusk3d@protonmail.ch>
2017-02-23 17:15:46 +0000

    alex d changes and update for end of day

--- src/main/java/qacinema/service/managers/offline/OfflineBookingManager.java
+++ src/main/java/qacinema/service/managers/offline/OfflineBookingManager.java
@@ -18,6 +18,7 @@ import qacinema.data.users.User;
 import qacinema.service.managers.BookingManager;
 import qacinema.testdata.TestData;
 
+
 @Default
 @Stateless
 @Loggable
@@ -36,9 +37,10 @@ public class OfflineBookingManager implements BookingManager {
 
 	@Override
 	public Booking findByBookingId(String bookingid) throws NoResultException {
-		for (Booking booking : testData.getBookingMap().values()) {
-			if (booking.getBookingid().equals(bookingid)) {
-				return booking;
+		Map<Integer, Booking> bookMap = testData.getBookingMap();
+		for (Entry<Integer, Booking> booking : bookMap.entrySet()) {
+			if (booking.getValue().getBookingid().equals(bookingid)) {
+				return booking.getValue();
 			}
 		}
 		throw new NoResultException("No matching BookingID found.");
@@ -48,8 +50,8 @@ public class OfflineBookingManager implements BookingManager {
 	@Override
 	public List<Booking> findByUser(User user) {
 		List<Booking> userBookings = new ArrayList<>();
-		for (Booking booking : testData.getBookingMap().values()) {
-			if (booking.getUser() == user) {
+		for (Booking booking : testData.getBookingMap().values()){
+			if(booking.getUser() == user){
 				userBookings.add(booking);
 			}
 		}
@@ -59,8 +61,8 @@ public class OfflineBookingManager implements BookingManager {
 	@Override
 	public List<Ticket> findAllTickets(Booking booking) {
 		List<Ticket> bookedTickets = new ArrayList<>();
-		for (Booking mapBooking : testData.getBookingMap().values()) {
-			if (mapBooking.equals(booking)) {
+		for(Booking mapBooking: testData.getBookingMap().values()){
+			if(mapBooking.equals(booking)){
 				bookedTickets.addAll(booking.getTickets());
 			}
 		}
@@ -70,8 +72,8 @@ public class OfflineBookingManager implements BookingManager {
 	@Override
 	public List<Booking> findByPaymentID(String paymentid) {
 		List<Booking> payBookings = new ArrayList<>();
-		for (Booking booking : testData.getBookingMap().values()) {
-			if (booking.getPayment().getPaymentsid().equals(paymentid)) {
+		for(Booking booking : testData.getBookingMap().values()){
+			if(booking.getPayment().getPaymentsid().equals(paymentid)){
 				payBookings.add(booking);
 			}
 		}
@@ -80,11 +82,10 @@ public class OfflineBookingManager implements BookingManager {
 
 	@Override
 	public void updateBooking(Booking bookingToUpdate) {
-		for (Booking booking : testData.getBookingMap().values()) {
-			if (booking.equals(bookingToUpdate)) {
-				// Enter code that updates it to whatever, probably need to
-				// overload this method
-				// so you can put in multiple things to update
+		for (Booking booking : testData.getBookingMap().values()){
+			if(booking.equals(bookingToUpdate)){
+				//Enter code that updates it to whatever, probably need to overload this method
+				//so you can put in multiple things to update
 			}
 		}
 
@@ -92,8 +93,8 @@ public class OfflineBookingManager implements BookingManager {
 
 	@Override
 	public void deleteBooking(Booking bookingToDelete) {
-		for (Booking booking : testData.getBookingMap().values()) {
-			if (booking.equals(bookingToDelete)) {
+		for (Booking booking : testData.getBookingMap().values() ){
+			if(booking.equals(bookingToDelete)){
 				testData.getBookingMap().remove(bookingToDelete.getBookingid());
 			}
 		}
@@ -102,8 +103,8 @@ public class OfflineBookingManager implements BookingManager {
 
 	@Override
 	public void addTicketToBooking(String bookingid, Ticket ticketToAdd) {
-		for (Booking booking : testData.getBookingMap().values()) {
-			if (booking.getBookingid().equals(bookingid)) {
+		for (Booking booking : testData.getBookingMap().values()){
+			if(booking.getBookingid().equals(bookingid)){
 				booking.addTicket(ticketToAdd);
 			}
 		}
@@ -111,12 +112,13 @@ public class OfflineBookingManager implements BookingManager {
 
 	@Override
 	public void removeTicketFromBooking(String bookingid, String ticketID) {
-		List<Ticket> bookedTickets = testData.getBookingMap().get(bookingid).getTickets();
-		for (Ticket ticket : bookedTickets) {
-			if (ticket.getTicketID().equals(ticketID)) {
-				bookedTickets.remove(ticketID);
+				List<Ticket> bookedTickets = testData.getBookingMap().get(bookingid).getTickets();
+				for (Ticket ticket : bookedTickets){
+					if(ticket.getTicketID().equals(ticketID)){
+						bookedTickets.remove(ticketID);
+					}
+				}
 			}
-		}
+		
+		
 	}
-
-}
