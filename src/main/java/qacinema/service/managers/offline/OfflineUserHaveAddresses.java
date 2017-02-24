2d2a8176463e63e206ad959f4133e38420786aef
mattimate <matthew-gordon@hotmail.co.uk>
2017-02-24 12:26:46 +0000

    Updated address managers that now have no connection to users

--- src/main/java/qacinema/service/managers/offline/OfflineUserHaveAddresses.java
+++ src/main/java/qacinema/service/managers/offline/OfflineUserHaveAddresses.java
@@ -1,12 +1,17 @@
 package qacinema.service.managers.offline;
 
+import javax.inject.Inject;
+
 import qacinema.data.users.Address;
 import qacinema.data.users.User;
 import qacinema.data.users.UserHaveAddresses;
 import qacinema.service.managers.UserHaveAddressesManager;
+import qacinema.testdata.TestData;
 
 public class OfflineUserHaveAddresses implements UserHaveAddressesManager {
 
+	@Inject private TestData testData;
+	
 	@Override
 	public UserHaveAddresses persistUserHaveAddresses(UserHaveAddresses userHaveAddresses) {
 		// TODO Auto-generated method stub
@@ -15,12 +20,10 @@ public class OfflineUserHaveAddresses implements UserHaveAddressesManager {
 
 	@Override
 	public Address findAddressByType(User user, String type) {
-		for (UserHaveAddresses userHaveAddresses : testData.getUserHaveAddressesMap().values()) {
-			if (userHaveAddresses.getUserEmail().equals(user.getEmail())) {
-				for (Address address : user.getAddressList()) {
-					if (userHaveAddresses.getAddressType().equals(type)) {
-						return address;
-					}
+		for (UserHaveAddresses userHaveAddresses : user.getUserHasAddresses()) {
+			for (Address address : testData.getAddressMap().values()) {
+				if (userHaveAddresses.getAddressType().equals(type)) {
+					return address;
 				}
 			}
 		}
