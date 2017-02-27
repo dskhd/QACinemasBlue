4c9b762da940cee6abc6d03d2ed8765051295c3e
Alex <alex_newton@btinternet.com>
2017-02-27 15:33:39 +0000

    Fixed mockito test and modified Utilities class

--- src/main/java/qacinema/service/managers/offline/QACinemaUtilities.java
+++ src/main/java/qacinema/service/managers/offline/QACinemaUtilities.java
@@ -4,11 +4,14 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class QACinemaUtilities {
+public final class QACinemaUtilities {
+	private QACinemaUtilities(){
+		// Empty constructor to override default constructor and prevent instantiation
+	}
+	
 	public static String getDate() {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Calendar cal = Calendar.getInstance();
-		String date = dateFormat.format(cal);
-		return date;
+		return dateFormat.format(cal);
 	}
 }
