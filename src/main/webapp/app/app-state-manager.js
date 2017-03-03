f367f75e6a4b5cff78966efc3bbe20deaa95498b
jetbooster <jarvisam@gmail.com>
2017-03-03 12:02:23 +0000

    merged in my code with newer index.html

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -7,18 +7,15 @@
 			$stateProvider.state("frontpage", {
 				url : "/frontpage",
 				templateUrl : "app/feature/frontpage/frontpage.html"
-<<<<<<< HEAD
-			}).state("ourScreen", {
-				url : "/ourScreen",
+			}).state("screens", {
+				url : "/screens",
 				templateUrl : "app/feature/ourscreens/ourScreens.html"
-=======
 			}).state("film", {
 				url : "/film",
 				templateUrl : "app/feature/film/film.html"
 			}).state("aboutus", {
 				url : "/about",
 				templateUrl : "app/feature/about/about.html"
->>>>>>> a0d5f1bb68218009ea5d90db303bca69c8892f18
 			})
 		});
 	
