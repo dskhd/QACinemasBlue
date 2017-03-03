7fe59a6abbff6edd9e17c7f1c42243ec3a4a8416
mattimate <matthew-gordon@hotmail.co.uk>
2017-03-03 10:43:23 +0000

    Classifaication webpage js functions

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -9,10 +9,13 @@
 				templateUrl : "app/feature/frontpage/frontpage.html"
 			}).state("film", {
 				url : "/film",
-				templateUrl : "app/feature/film/film.html"
+				templateUrl : "app/feature/book/film/film.html"
 			}).state("aboutus", {
 				url : "/aboutus",
 				templateUrl : "app/feature/aboutus/aboutus.html"
+			}).state("classification", {
+				url : "/classification",
+				templateUrl : "app/feature/classification/classification.html"
 			})
 		});
 }());
