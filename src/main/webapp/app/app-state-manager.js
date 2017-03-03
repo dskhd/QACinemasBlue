dbc95219087c3988f0f21989a759adcaf7e4342c
jetbooster <jarvisam@gmail.com>
2017-03-03 11:41:21 +0000

    viewified OurScreens

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -7,6 +7,10 @@
 			$stateProvider.state("frontpage", {
 				url : "/frontpage",
 				templateUrl : "app/feature/frontpage/frontpage.html"
+			}).state("ourScreen", {
+				url : "/ourScreen",
+				templateUrl : "app/feature/ourscreens/ourScreens.html"
 			})
 		});
+	
 }());
